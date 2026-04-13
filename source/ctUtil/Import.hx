824df312daee4a40638f9dcd8409507d74a716bd
comatoasterstrudel <123904583+comatoasterstrudel@users.noreply.github.com>
2026-04-12 20:58:46 -0500

    use ctUtil as Haxelib

--- source/ctUtil/Import.hx
+++
@@ -1,9 +0,0 @@
-package ctUtil;
-
-import ctUtil.*;
-import ctUtil.extensions.*;
-import ctUtil.json.*;
-import ctUtil.lerp.*;
-import flixel.util.FlxDirection;
-import haxe.Json;
-import openfl.Assets;
